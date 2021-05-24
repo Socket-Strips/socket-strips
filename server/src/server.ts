efa6ffe6a521fbc0f0d3039f7a7eebd9833074a1
Rasync <gamespeterplelit@gmail.com>
2021-05-24 21:29:49 +0100

    t2

--- server/src/server.ts
+++ server/src/server.ts
@@ -32,13 +32,13 @@ if (process.env["NODE_ENV"] !== "production") {
   config();
 }
 
-const PORT = Number(process.env["$PORT"]) || 3001;
+const PORT = Number(process.env["PORT"]) || 3001;
 
 const io = new Server(PORT, {
   cors: { origin: process.env["CORS_ORIGIN"] },
 });
 
-console.log(`$PORT=${process.env["$PORT"]}`);
+console.log(`PORT=${process.env["PORT"]}`);
 console.log(`Listening on: ${PORT}`);
 logger.info(`Listening on: ${PORT}`);
 
